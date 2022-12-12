4e19b46e09b8bdf002bec717fd5ea915bef0d3f9
yoma <codeyoma@gmail.com>
2022-12-12 17:41:31 +0900

    [add] cint

--- syntax/syntax.hpp
+++ syntax/syntax.hpp
@@ -3,9 +3,18 @@
 #define __SYNTAX_HPP__
 
 #include <cstddef>
-#include <cstdint>
 #include <cstring>
 #include <string>
+
+// #include <cstdint>
+typedef signed char		   int8_t;
+typedef unsigned char	   uint8_t;
+typedef short			   int16_t;
+typedef unsigned short	   uint16_t;
+typedef int				   int32_t;
+typedef unsigned int	   uint32_t;
+typedef long long		   int64_t;
+typedef unsigned long long uint64_t;
 
 typedef enum {
 	spx_error = -1,
@@ -32,5 +41,8 @@ spx_chunked_syntax_data_line(std::string const& line,
 							 std::string&		data_store,
 							 std::string&		trailer_section,
 							 uint8_t&			trailer_count);
+
+status
+spx_config_syntax(std::string const& line, );
 
 #endif
