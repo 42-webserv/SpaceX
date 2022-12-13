80c3e5949ae00093319a97d3e410b1aa48aeeb65
Woo Joo Chae <enaenen@naver.com>
2022-12-14 01:41:06 +0900

    tests

---
+++ response/echo_client.cpp
@@ -0,0 +1,67 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+#define SERV_IP "127.0.0.1"
+#define SERV_PORT 80
+
+void error_handling(char* message);
+
+int
+main(int argc, char** argv) {
+	int				   sock;
+	struct sockaddr_in serv_addr;
+	char			   message[BUFSIZE];
+	int				   str_len;
+
+	// if ( argc != 3 ) {
+	// 	printf( "Usage : %s <IP> <port> \n", argv[0] );
+	// 	exit( 1 );
+	// }
+
+	sock = socket(PF_INET, SOCK_STREAM, 0); /* 서버 접속을 위한 소켓 생성 */
+	if (sock == -1) {
+		error_handling("socket() error");
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(SERV_IP);
+	serv_addr.sin_port = htons(SERV_PORT);
+
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+		error_handling("connect() error");
+	}
+
+	while (1) {
+		/* 메시지 입력 전송*/
+		fputs("전송할 메시지를 입력하세요(q to quit) : ", stdout);
+		fgets(message, BUFSIZE, stdin);
+		if (!strcmp(message, "q\n")) {
+			break;
+		}
+		// char *message =
+		// 	"GET / HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: "
+		// 	"Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n";
+		write(sock, message, strlen(message));
+
+		/* 메시지 수신 출력 */
+		str_len = read(sock, message, BUFSIZE - 1);
+		message[str_len] = 0;
+		printf("서버로부터 전송된 메시지 : %s \n", message);
+	}
+	close(sock);
+	return 0;
+}
+
+void
+error_handling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
--- response/response.hpp
+++ response/response.hpp
@@ -2,26 +2,66 @@
 #define __RESPONSE_HPP__
 
 #include <algorithm>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+
+#include "response_form.hpp"
 
 struct Response {
 	typedef std::pair<std::string, std::string> header;
 
 private:
 	std::vector<header> headers_;
-	int					versionMinor_;
-	int					versionMajor_;
+	int					version_minor_;
+	int					version_major_;
 	std::vector<char>	body_;
-	bool				keepAlive_;
-	unsigned int		statusCode_;
+	bool				keep_alive_;
+	unsigned int		status_code_;
 	std::string			status_;
 
 	std::string
 	make() const {
 		std::stringstream stream;
-		stream << "HTTP/" << versionMajor_ << "." << versionMinor_ << " " << status_
+		stream << "HTTP/" << 1 << "." << 1 << " " << status_
+			   << "\r\n";
+		for (std::vector<Response::header>::const_iterator it = headers_.begin();
+			 it != headers_.end(); ++it)
+			stream << it->first << ": " << it->second << "\r\n";
+		std::string data(body_.begin(), body_.end());
+		stream << data << "\r\n";
+		return stream.str();
+	}
+
+	void
+	generateStatus_(enum http_status status) {
+		std::stringstream stream;
+		stream << status << " " << http_status_sstr(status) << "\r\n";
+		status_ = stream.str();
+	};
+
+public:
+	// constructor
+	Response()
+		: version_minor_(1)
+		, version_major_(1)
+		, status_code_(HTTP_STATUS_BAD_REQUEST) {
+		generateStatus_(HTTP_STATUS_BAD_REQUEST);
+	};
+
+	Response(enum http_status status)
+		: version_minor_(1)
+		, version_major_(1)
+		, status_code_(status) {
+		generateStatus_(status);
+	};
+
+	// make
+	std::string
+	make_test() {
+		std::stringstream stream;
+		stream << "HTTP/" << version_major_ << "." << version_minor_ << " " << status_
 			   << "\r\n";
 		for (std::vector<Response::header>::const_iterator it = headers_.begin();
 			 it != headers_.end(); ++it)
@@ -31,5 +71,29 @@ private:
 		return stream.str();
 	}
 };
+
+/*
+int main( void ) {
+	std::cout << HTTP_STATUS_BAD_REQUEST << std::endl;
+	std::cout << http_status_str( HTTP_STATUS_NOT_ACCEPTABLE ) << std::endl;
+}
+
+HTTP/1.1 400 Bad Request
+Server: nginx/1.23.3
+Date: Tue, 13 Dec 2022 15:45:26 GMT
+Content-Type: text/html
+Content-Length: 157
+Connection: close
+
+<html>
+<head><title>400 Bad Request</title></head>
+<body>
+<center><h1>400 Bad Request</h1></center>
+<hr><center>nginx/1.23.3</center>
+</body>
+</html>
+
+
+*/
 
 #endif
