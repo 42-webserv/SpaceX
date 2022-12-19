60985998c52f503fee2c6d3e6202d18eb1c95e1c
Woo Joo Chae <enaenen@naver.com>
2022-12-20 02:45:49 +0900

    refactor response structures

--- response/response.hpp
+++ response/response.hpp
@@ -12,16 +12,16 @@
 #include <vector>
 
 #include "../dev_folder/ClientBuffer.hpp"
-#include "client_buffer.hpp"
+#include "ClientBuffer.hpp"
+#include "error_pages.hpp"
 #include "response_form.hpp"
+
 // tmp for add_event_chagne
 #include "tmp_kqueue_headers.hpp"
 
 #define SERVER_HEADER_KEY "Server"
 #define SERVER_HEADER_VALUE "SpaceX"
-#define SERVER_HEADER_CONTENT_LENGTH "Content-Legnth"
-#define TEST_FILE "html/test.html"
-
+#define CONTENT_LENGTH "Content-Legnth"
 #define CONTENT_TYPE "Content-Type";
 
 #define MIME_TYPE_HTML "text/html"
@@ -31,6 +31,13 @@
 #define MIME_TYPE_BMP "image/bmp"
 #define MIME_TYPE_TEXT "text"
 #define MIME_TYPE_DEFUALT "application/octet-stream"
+
+// This must change to file that loaded from CONFIG file
+#define ERR_PAGE_URL "html/404error.html"
+
+#define LF (u_char)'\n'
+#define CR (u_char)'\r'
+#define CRLF "\r\n"
 
 struct Response {
 	typedef std::pair<std::string, std::string> header;
@@ -44,100 +51,54 @@ private:
 	bool				keep_alive_ = true;
 
 	std::string
-	make() const {
+	make_to_string() const {
 		std::stringstream stream;
-		stream << "HTTP/" << version_major_ << "." << version_minor_ << " " << status_
-			   << "\r\n";
+		stream << "HTTP/" << version_major_ << "." << version_minor_ << " " << status_code_ << " " << status_
+			   << CRLF;
 		for (std::vector<Response::header>::const_iterator it = headers_.begin();
 			 it != headers_.end(); ++it)
-			stream << it->first << ": " << it->second << "\r\n";
+			stream << it->first << ": " << it->second << CRLF;
 		return stream.str();
 	}
-	/*
-	Server: nginx/1.23.3
-	Date: Tue, 13 Dec 2022 15:45:26 GMT
-	Content-Type: text/html
-	Content-Length: 157
-	Connection: close
-	*/
-	void
-	generateStatus_(enum http_status status) {
-		std::stringstream stream;
-		stream << status << " " << http_status_sstr(status);
-		status_ = stream.str();
-	};
-
-public:
-	// constructor
-	Response()
-		: version_minor_(1)
-		, version_major_(1)
-		, status_code_(HTTP_STATUS_BAD_REQUEST) {
-		generateStatus_(HTTP_STATUS_OK);
-	};
-
-	Response(enum http_status status)
-		: version_minor_(1)
-		, version_major_(1)
-		, status_code_(status) {
-		generateStatus_(status);
-	};
+	// Response Header EXAMPLE
+	// HTTP/1.1 400 Bad Request
+	// Server: nginx/1.23.3
+	// Date: Tue, 13 Dec 2022 15:45:26 GMT
+	// Content-Type: text/html
+	// Content-Length: 157
+	// Connection: close
 
 	int
-	setContentLength_fileOpen(std::vector<struct kevent>& change_list, ClientBuffer& client_buffer) {
-		int fd = open(client_buffer.req_res_queue_.front().first.req_target_.c_str(), O_RDONLY);
-
-		// when URL file open failed
-		if (fd < 0) {
-			// TODO : load 404 file from confing
-			fd			 = open("html/error.html", O_RDONLY);
-			off_t length = lseek(fd, 0, SEEK_END);
-			lseek(fd, SEEK_CUR, SEEK_SET);
-
-			std::stringstream ss;
-			ss << length;
-
-			headers_.push_back(header(SERVER_HEADER_CONTENT_LENGTH, ss.str()));
-		}
-		// change_list, fd, EVFILT_READ OR WRITE<status>, EV_ADD | EV_ENABLE <actions>, 0, 0, t_client_buf<udata>
-		add_change_list(change_list, fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, &client_buffer);
+	file_open(const char* dir) {
+		int fd = open(dir, O_RDONLY);
+		if (fd < 0)
+			return open(ERR_PAGE_URL, O_RDONLY);
 	}
 
 	void
-	generateCommonHeader() {
-		headers_.push_back(header(SERVER_HEADER_KEY, SERVER_HEADER_VALUE));
+	setContentLength(int fd) {
+		off_t length = lseek(fd, 0, SEEK_END);
+		lseek(fd, SEEK_CUR, SEEK_SET);
 
-		// headers_.push_back(header());
-	};
+		std::stringstream ss;
+		ss << length;
 
-	// TODO : 분기 생성
-	/*
-	BAD REQUEST 이거나 config가 비었을경우 text/html
-	application/octet-stream => out 파일일경우 (8비트파일)
+		headers_.push_back(header(CONTENT_LENGTH, ss.str()));
+	}
 
-	*/
 	void
-	setContentType(t_req_field& req) {
-
-		// bad request or errorpage exceptions
-		if (req.body_flag_ >= HTTP_STATUS_BAD_REQUEST) {
-			headers_.push_back(header("Content-Type", MIME_TYPE_HTML));
-			keep_alive_ = false;
-			return;
-		}
+	setContentType(std::string uri) {
 
 		// request extention checking
-		std::string			   uri			= req.req_target_;
 		std::string::size_type uri_ext_size = uri.find_last_of('.');
 		std::string			   ext;
 
 		if (uri_ext_size)
 			ext = uri.substr(uri_ext_size + 1);
-
-		if (ext == "png")
+		if (ext == "html")
+			headers_.push_back(header("Content-Type", MIME_TYPE_HTML));
+		else if (ext == "png")
 			headers_.push_back(header("Content-Type", MIME_TYPE_PNG));
-		else if (ext == "bmp")
-			headers_.push_back(header("Content-Type", MIME_TYPE_BMP));
 		else if (ext == "jpg")
 			headers_.push_back(header("Content-Type", MIME_TYPE_JPG));
 		else if (ext == "jpeg")
@@ -148,92 +109,45 @@ public:
 			headers_.push_back(header("Content-Type", MIME_TYPE_DEFUALT));
 	};
 
-// this define will be replace to config file settings input
-#define ERR_PAGE_URL "html/error.html"
-
 	std::string
-	make(std::vector<struct kevent>& change_list, ClientBuffer& client_buffer) {
-		t_req_field cur_req = client_buffer.req_res_queue_.front().first;
-
-		setContentType(cur_req);
-		setContentLength_fileOpen(change_list, client_buffer);
-
-		std::stringstream stream;
-		stream << "HTTP/" << version_major_ << "." << version_minor_ << " " << status_
-			   << "\r\n";
-		for (std::vector<Response::header>::const_iterator it = headers_.begin();
-			 it != headers_.end(); ++it)
-			stream << it->first << ": " << it->second << "\r\n";
-		stream << "\r\n";
-		return stream.str();
-	};
-
-	std::string
-	makeBadRequestResponse(ClientBuffer& client_buffer) {
+	handle_static_error_page() {
+		char* err = http_error_400_page;
+		// this will write to vector<char> response_body
+		return make_to_string();
 	}
 
-	// make 분리
-	//  TODO : 요청에 따라 BODY 여부 확인
-	//  TODO : 요청내용이 어떻게 들어오는지 확인
-	//  TODO : 요청에 따른 필수 Header key value 셋팅
-	/*
-		void
-		setBody(const char* dir) {
+public:
+	std::string
+	make_error_response(std::vector<struct kevent>& change_list, ClientBuffer& client_buffer, http_status error_code) {
 
-			int fd = open(dir, O_RDONLY);
-			if (fd < 0)
-				std::cout << "fd error" << std::endl;
-			off_t length = lseek(fd, 0, SEEK_END);
-			off_t i		 = 0;
-			char  buf[1024];
+		status_		 = http_status_sstr(error_code);
+		status_code_ = error_code;
 
-			std::stringstream ss;
-			ss << length;
-			headers_.push_back(header(SERVER_HEADER_CONTENT_LENGTH, ss.str()));
+		if (error_code == HTTP_STATUS_BAD_REQUEST)
+			keep_alive_ = false;
 
-			std::cout << read(fd, buf, 1024)
-					  << std::endl;
-			int x = 0;
-			while (buf[x]) {
-				std::cout << *buf << std::endl;
-				x++;
-			}
+		int error_req_fd = open(ERR_PAGE_URL, O_RDONLY);
+		if (error_req_fd < 0)
+			return handle_static_error_page();
+		add_change_list(change_list, error_req_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, &client_buffer);
+	}
 
-			for (off_t i = 0; i < length; i++) {
-				body_.push_back(buf[i]);
-			}
-			for (std::vector<char>::iterator it = body_.begin(); it != body_.end(); it++)
-				std::cout << *it;
-			std::cout << "\n";
-		}
-	*/
+	// this is main logic to make response
+	std::string
+	setting_response_header(std::vector<struct kevent>& change_list, ClientBuffer& client_buffer) {
+		t_req_field current_request = client_buffer.req_res_queue_.front().first;
+		std::string uri				= current_request.req_target_;
+		int			request_fd		= file_open(uri.c_str());
+
+		// File Not Found - URL
+		if (request_fd < 0)
+			return make_error_response(change_list, client_buffer, HTTP_STATUS_NOT_FOUND);
+		setContentType(uri);
+		setContentLength(request_fd);
+		// body event register
+		add_change_list(change_list, request_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, &client_buffer);
+		return make_to_string();
+	}
 };
-
-/*
-
-HTTP/1.1 400 Bad Request
-Server: nginx/1.23.3
-Date: Tue, 13 Dec 2022 15:45:26 GMT
-Content-Type: text/html
-Content-Length: 157
-Connection: close
-
-<html>
-<head><title>400 Bad Request</title></head>
-<body>
-<center><h1>400 Bad Request</h1></center>
-<hr><center>nginx/1.23.3</center>
-</body>
-</html>
-
-	const char* str = "<html>\r\n"
-					  "<head><title>400 Bad Request</title></head>\r\n"
-					  "<body>\r\n"
-					  "<center><h1>400 Bad Request</h1></center>\r\n"
-					  "<hr><center>nginx/1.23.3</center>\r\n"
-					  "</body>\r\n"
-					  "</html>\r\n";
-
-*/
 
 #endif
