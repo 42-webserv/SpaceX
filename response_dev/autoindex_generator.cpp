993305b24faa1aec6dd39bc57ada648a05eab496
enaenen <enaenen@naver.com>
2022-12-22 17:57:11 +0900

    auto index v1

---
+++ response_dev/autoindex_generator.cpp
@@ -0,0 +1,80 @@
+#include <dirent.h>
+#include <iostream>
+#include <libgen.h> //basename
+#include <string>
+#include <sys/stat.h>
+
+#define LF '\n'
+#define CR '\r'
+#define CRLF "\r\n"
+
+#define HTML_HEAD_TITLE "<html>\r\n<head><title>Index of "
+#define HTML_HEAD_TO_BODY "</title></head>\r\n<body>\r\n<h1>Index of "
+#define HTML_BEFORE_LIST "</h1><hr>\r\n<pre>\r\n"
+#define HTML_AFTER_LIST "</pre>\r\n<hr>\r\n</body>\r\n</html>"
+#define TD_STYLE "style=\"padding-left: 500px;padding-right: 100px;\""
+
+#define A_TAG_START "<a href=\""
+#define A_TAG_END "\">"
+#define CLOSE_A_TAG "</a>"
+
+void
+generate(const char* path) {
+	// std::stringstream stream;
+	DIR*		   dir;
+	struct dirent* entry;
+
+	std::string full_path("/");
+	full_path + path;
+	char* base_name = basename((char*)full_path.c_str());
+	std::cout << HTML_HEAD_TITLE << base_name << HTML_HEAD_TO_BODY << base_name
+			  << HTML_BEFORE_LIST;
+	std::cout << "<table>";
+	if ((dir = opendir(path)) != NULL) {
+		/* print all the files and directories within directory */
+		while ((entry = readdir(dir)) != NULL) {
+			// get the name of the file
+			std::string filename = entry->d_name;
+			if (filename.compare(".") == 0)
+				continue;
+			std::cout << "<tr>";
+			// get the full path of the file
+			std::cout << "<td>";
+			std::cout << A_TAG_START << entry->d_name << A_TAG_END;
+
+			if (filename.compare("..") == 0) {
+				std::cout << filename << CLOSE_A_TAG << CRLF;
+				continue;
+			}
+			std::string full_path = path + filename;
+			struct stat file_status;
+			if (stat(full_path.c_str(), &file_status) == 0) {
+				// get the size of the file in bytes
+				size_t size = file_status.st_size;
+				// get the time of the last modification
+				time_t	 mtime		 = file_status.st_mtime;
+				std::tm* modify_time = std::gmtime(&mtime);
+
+				char date_buf[32];
+				std::strftime(date_buf, sizeof(date_buf), "%d-%b-%Y %H:%M",
+							  modify_time);
+				// print the name, size, and modification time
+				std::cout << filename << CLOSE_A_TAG << "</td>"
+						  << "<td " << TD_STYLE << ">" << date_buf << "</td><td>"
+						  << size << "</td>" << CRLF;
+			}
+		}
+		std::cout << "</table>" << HTML_AFTER_LIST;
+		closedir(dir);
+	} else {
+		std::cout << "error" << std::endl;
+		/* could not open directory */
+		perror("");
+	}
+}
+
+int
+main(void) {
+	generate("/Users/spacechae/Desktop/webserv/wchae_practice/"
+			 "auto_index_feature_test/");
+}
--- source/spx_response_generator.cpp
+++ source/spx_response_generator.cpp
@@ -55,7 +55,7 @@ Response::setContentType(std::string uri) {
 
 std::string
 Response::handle_static_error_page() {
-	char* err = http_error_400_page;
+	// char* err = http_error_400_page;
 	// this will write to vector<char> response_body
 	return make_to_string();
 }
@@ -93,7 +93,7 @@ std::string
 Response::setting_response_header(std::vector<struct kevent>& change_list, ClientBuffer& client_buffer) {
 	t_req_field current_request = client_buffer.req_res_queue_.front().first;
 	std::string uri				= current_request.req_target_;
-	int			request_fd		= file_open(client_buffer.req_res_queue_.front().second.file_path_.c_str());
+	int			request_fd		= file_open(uri.c_str());
 
 	// File Not Found - URL
 	if (request_fd < 0)
@@ -102,7 +102,6 @@ Response::setting_response_header(std::vector<struct kevent>& change_list, Clien
 	setContentLength(request_fd);
 	headers_.push_back(header(CONNECTION, KEEP_ALIVE));
 	// body event register
-
 	add_change_list(change_list, request_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, &client_buffer);
 	return make_to_string();
 }
