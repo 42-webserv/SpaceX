fd82f03f45a14bfbd205b944241d8d8a86090fa6
enaenen <enaenen@naver.com>
2022-12-25 21:36:56 +0900

    unit_test

---
+++ response_dev/unit_test/spx_parse_config.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef __SPX__PARSE__CONFIG__HPP__
+#define __SPX__PARSE__CONFIG__HPP__
+
+#include "spx_core_type.hpp"
+#include "spx_core_util_box.hpp"
+#include "spx_port_info.hpp"
+#include <cctype>
+#include <cstring>
+#include <map>
+#include <string>
+#include <vector>
+
+status
+spx_config_syntax_checker(std::string const&	   buf,
+						  total_port_server_map_p& config_map,
+						  std::string const&	   cur_path);
+
+#endif
